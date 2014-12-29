c7431b66cb885ffb923217e004b590acfdf093bf
Nils Kellermann <nils.kellermann@mni.thm.de>
2014-12-29 15:57:35 +0100

    erster commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Test-Seite</title>
+<style>
+#impressum {
+	position:fixed;
+    bottom:0px;
+}
+</style>
+</head>
+<body>
+<body background="background.png" link="#0000CC" vlink="#000066" alink="#000000">
+<h1>Test</h1>
+
+<p>Dies ist eine ganz einfache Homepage.
+</p>
+
+<div id="impressum">
+
+
+<p>
+<b>Impressum</b> <br>
+Nils Kellermann <br>
+Frankenstra&szlig;e 12 <br>
+35510 Pohl-G&ouml;ns <br>
+nilskellermann@web.de <br>
+Tel: 06033 73444
+</p>
+</div>
+</body>
+</html>
